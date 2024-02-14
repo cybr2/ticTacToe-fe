5c26a3cdcaec18db45689cf24cf8cfa3de06c88b
cybr2 <ydoowbernisca2@gmail.com>
2024-02-15 05:13:20 +0800

    add all dependencies

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-cyan-100 flex h-screen">
+      Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
